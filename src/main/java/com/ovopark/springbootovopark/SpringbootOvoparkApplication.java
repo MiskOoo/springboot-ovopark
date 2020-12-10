7d60356c32e98fa2984368e9d38c9b2e01319390
Misko <402301200@qq.com>
2020-12-10 14:51:50 +0800

    init

---
+++ src/main/java/com/ovopark/springbootovopark/SpringbootOvoparkApplication.java
@@ -0,0 +1,13 @@
+package com.ovopark.springbootovopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOvoparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOvoparkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ovopark/springbootovopark/SpringbootOvoparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovopark.springbootovopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOvoparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
